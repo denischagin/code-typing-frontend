1b4574725c0d4fc4882e8a162380ca6b020ed0a9
denischagin <denischaginnn@gmail.com>
2024-01-03 02:05:34 +0300

    feat: завернул в докер

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+        port: 3000,
+    },
     resolve: {
         alias: {
             '@app': path.resolve(__dirname, './src/app'),
